d1d9cd822b7cd9c3263a52ba9f3245ed3e497d74
ayldhaa <1151044240@qq.com>
2020-08-29 14:12:52 +0800

    8.29  上传

--- src/main/java/com/aaa/Security/CustomerSecurity.java
+++ src/main/java/com/aaa/Security/CustomerSecurity.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -129,5 +130,10 @@ public class CustomerSecurity extends WebSecurityConfigurerAdapter {
         daoAuthenticationProvider.setUserDetailsService(customerConfig);
         auth.authenticationProvider(daoAuthenticationProvider);
 
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/static/**");
     }
 }
