f5d94fc5c3e3d38c2e56606c2f921489c0c7b925
ayldhaa <1151044240@qq.com>
2020-08-29 14:04:27 +0800

    8.29  上传

--- src/main/java/com/aaa/Dao/Car_seriesDao.java
+++ src/main/java/com/aaa/Dao/Car_seriesDao.java
@@ -2,9 +2,10 @@ package com.aaa.Dao;
 
 import com.aaa.Entity.Car_series;
 import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface Car_seriesDao {
 
     //查询汽车系列
@@ -12,4 +13,7 @@ public interface Car_seriesDao {
 
     //分页查询
     PageInfo<Car_series> showCarSericePage(Integer pageNum,Integer pageSize);
+
+    List<Car_series> showCar_series(String full_name);
+
 }
--- src/main/java/com/aaa/Dao/Car_series_groupDao.java
+++ src/main/java/com/aaa/Dao/Car_series_groupDao.java
@@ -2,9 +2,11 @@ package com.aaa.Dao;
 
 import com.aaa.Entity.Car_series_group;
 import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface Car_series_groupDao {
 
     // 查询汽车系列
